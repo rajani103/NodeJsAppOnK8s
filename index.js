3bfa8777f8496074977bc7a72d9010ab8870f440
Rajani Ekunde <43741838+rajani103@users.noreply.github.com>
2023-06-12 19:11:13 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,14 +2,14 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-    res.send('{ "response": "Hello From Thetips4you" }');
+    res.send('{ "response": "Hello From misalPav" }');
 });
 
 app.get('/will', function (req, res) {
-    res.send('{ "response": "Hello World" }');
+    res.send('{ "response": "Hello misalPav" }');
 });
 app.get('/ready', function (req, res) {
-    res.send('{ "response": " Great!, It works!" }');
+    res.send('{ "response": " Great!, It works! yooo" }');
 });
 app.listen(process.env.PORT || 3000);
 module.exports = app;
